48fa2f8c3b0f6d0a8717826ca6c154a4b09eb2bf
Amil Masic <amil.masic@gmail.com>
2018-09-01 14:17:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,13 @@
 class Dog
 
-  # def initialize(name)
-  #   @this_dog_name = name
-  # end
-  #
-  # def name=(name)
-  #   @this_dog_name = name
-  # end
-  # def name
-  #   "#{@name}"
-  # end
-  # fido = Dog.new("Fido")
-  # fido.name
-  def name= (name)
-    @this_name = name
+  def initialize(name)
+    @name = name
   end
+
   def name
-    @this_name
+    @name
   end
 
 end
-fido = Dog.new
-fido.name = fido
+
+fido = Dog.new("Fido")
